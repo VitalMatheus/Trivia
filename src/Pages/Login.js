77ad90bc8e9b0aab103279c7165b62101a3233b6
João Victor Soares <joaovictorcpsa@gmail.com>
2022-04-05 16:20:49 -0300

    Requisito 1 - feito

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,54 @@
 import React from 'react';
 
+const MIN_VALUE_INPUT = 1;
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { name, email } = this.state;
     return (
       <section>
-        <label htmlFor="">
-          Nome:
-          <input
-            type="text"
-            
-            />
-        </label>
+        <input
+          name="name"
+          data-testid="input-player-name"
+          type="text"
+          id="inputName"
+          placeholder="Digite seu nome"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="email"
+          name="email"
+          data-testid="input-gravatar-email"
+          placeholder="Digite seu email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ name.length < MIN_VALUE_INPUT || email.length < MIN_VALUE_INPUT }
+        >
+          Play
+        </button>
       </section>
     );
   }
 }
 
 export default Login;
-//push
