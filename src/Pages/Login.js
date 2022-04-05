76555ea1aef4fcbf5a8f94b97611cc0347e30a08
João Victor Soares <joaovictorcpsa@gmail.com>
2022-04-05 15:28:24 -0300

    Adiciona BrowserRouter e Redux

---
+++ src/Pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <section>
+        <label htmlFor="">
+          Nome: 
+          <input
+            type="text" />
+        </label>
+      </section>
+    );
+  }
+}
+
+export default Login;
