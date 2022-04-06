8be8e1d46bfb0cddb6c851245d8c15dccd5e7f57
Débora Serra <debora.r.serra@gmail.com>
2022-04-06 10:54:25 -0300

    Tira a logo

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import Game from './Pages/Game';
 import Login from './Pages/Login';
 import Ranking from './Pages/Ranking';
 import Settings from './Pages/Settings';
-import logo from './trivia.png';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/feedback" component={ Feedback } />
